fae450b9be6951c3190c04a4279b1ef5755087d8
zzy0202 <57013433+zzy0202@users.noreply.github.com>
2021-10-03 02:20:22 +0800

    Update test.java

--- test.java
+++ test.java
@@ -24,8 +24,8 @@ public class test {
         hashMap.put("==","Eq");
     }
     public static void main(String[] args) throws IOException {
-        FileReader fileReader = new FileReader("C:\\Users\\yung\\IdeaProjects\\compilers\\src\\test.txt");
-//        FileReader fileReader = new FileReader(args[0]);
+//        FileReader fileReader = new FileReader("C:\\Users\\yung\\IdeaProjects\\compilers\\src\\test.txt");
+        FileReader fileReader = new FileReader(args[0]);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         initial();
         while(true){
