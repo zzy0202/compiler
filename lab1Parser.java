f25137b660326bc29cfde7612eea50f693f324b0
zzy0202 <57013433+zzy0202@users.noreply.github.com>
2021-10-20 07:24:16 -0700

    Add files via upload

---
+++ lab1Parser.java
@@ -0,0 +1,454 @@
+package lab1;// Generated from lab1.g4 by ANTLR 4.9.2
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.ATN;
+import org.antlr.v4.runtime.atn.ATNDeserializer;
+import org.antlr.v4.runtime.atn.ParserATNSimulator;
+import org.antlr.v4.runtime.atn.PredictionContextCache;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class lab1Parser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.9.2", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, T__6=7, T__7=8, WhiteSpace=9, 
+		DecimalConst=10, OctalConst=11, HexadecimalConst=12, HexadecimalPrefix=13, 
+		NonzeroDigit=14, OctalDigit=15, Digit=16, HexadecimalDigit=17;
+	public static final int
+		RULE_compUnit = 0, RULE_funcDef = 1, RULE_funcType = 2, RULE_ident = 3, 
+		RULE_block = 4, RULE_stmt = 5, RULE_number = 6;
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"compUnit", "funcDef", "funcType", "ident", "block", "stmt", "number"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "'('", "')'", "'int'", "'main'", "'{'", "'}'", "'return'", "';'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, null, null, null, null, null, null, null, null, "WhiteSpace", "DecimalConst", 
+			"OctalConst", "HexadecimalConst", "HexadecimalPrefix", "NonzeroDigit", 
+			"OctalDigit", "Digit", "HexadecimalDigit"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "lab1.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public lab1Parser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	public void addErrorListener() {
+	}
+
+	public void removeErrorListener() {
+	}
+
+	public static class CompUnitContext extends ParserRuleContext {
+		public FuncDefContext funcDef() {
+			return getRuleContext(FuncDefContext.class,0);
+		}
+		public CompUnitContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_compUnit; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).enterCompUnit(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).exitCompUnit(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof lab1Visitor ) return ((lab1Visitor<? extends T>)visitor).visitCompUnit(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final CompUnitContext compUnit() throws RecognitionException {
+		CompUnitContext _localctx = new CompUnitContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_compUnit);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(14);
+			funcDef();
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class FuncDefContext extends ParserRuleContext {
+		public FuncTypeContext funcType() {
+			return getRuleContext(FuncTypeContext.class,0);
+		}
+		public IdentContext ident() {
+			return getRuleContext(IdentContext.class,0);
+		}
+		public BlockContext block() {
+			return getRuleContext(BlockContext.class,0);
+		}
+		public FuncDefContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_funcDef; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).enterFuncDef(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).exitFuncDef(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof lab1Visitor ) return ((lab1Visitor<? extends T>)visitor).visitFuncDef(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final FuncDefContext funcDef() throws RecognitionException {
+		FuncDefContext _localctx = new FuncDefContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_funcDef);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(16);
+			funcType();
+			setState(17);
+			ident();
+			setState(18);
+			match(T__0);
+			setState(19);
+			match(T__1);
+			setState(20);
+			block();
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class FuncTypeContext extends ParserRuleContext {
+		public FuncTypeContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_funcType; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).enterFuncType(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).exitFuncType(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof lab1Visitor ) return ((lab1Visitor<? extends T>)visitor).visitFuncType(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final FuncTypeContext funcType() throws RecognitionException {
+		FuncTypeContext _localctx = new FuncTypeContext(_ctx, getState());
+		enterRule(_localctx, 4, RULE_funcType);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(22);
+			match(T__2);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class IdentContext extends ParserRuleContext {
+		public IdentContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_ident; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).enterIdent(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).exitIdent(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof lab1Visitor ) return ((lab1Visitor<? extends T>)visitor).visitIdent(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final IdentContext ident() throws RecognitionException {
+		IdentContext _localctx = new IdentContext(_ctx, getState());
+		enterRule(_localctx, 6, RULE_ident);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(24);
+			match(T__3);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class BlockContext extends ParserRuleContext {
+		public StmtContext stmt() {
+			return getRuleContext(StmtContext.class,0);
+		}
+		public BlockContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_block; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).enterBlock(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).exitBlock(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof lab1Visitor ) return ((lab1Visitor<? extends T>)visitor).visitBlock(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final BlockContext block() throws RecognitionException {
+		BlockContext _localctx = new BlockContext(_ctx, getState());
+		enterRule(_localctx, 8, RULE_block);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(26);
+			match(T__4);
+			setState(27);
+			stmt();
+			setState(28);
+			match(T__5);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class StmtContext extends ParserRuleContext {
+		public NumberContext number() {
+			return getRuleContext(NumberContext.class,0);
+		}
+		public StmtContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_stmt; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).enterStmt(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).exitStmt(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof lab1Visitor ) return ((lab1Visitor<? extends T>)visitor).visitStmt(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final StmtContext stmt() throws RecognitionException {
+		StmtContext _localctx = new StmtContext(_ctx, getState());
+		enterRule(_localctx, 10, RULE_stmt);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(30);
+			match(T__6);
+			setState(31);
+			number();
+			setState(32);
+			match(T__7);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class NumberContext extends ParserRuleContext {
+		public TerminalNode DecimalConst() { return getToken(lab1Parser.DecimalConst, 0); }
+		public TerminalNode OctalConst() { return getToken(lab1Parser.OctalConst, 0); }
+		public TerminalNode HexadecimalConst() { return getToken(lab1Parser.HexadecimalConst, 0); }
+		public NumberContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_number; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).enterNumber(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof lab1Listener ) ((lab1Listener)listener).exitNumber(this);
+		}
+		@Override
+		public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+			if ( visitor instanceof lab1Visitor ) return ((lab1Visitor<? extends T>)visitor).visitNumber(this);
+			else return visitor.visitChildren(this);
+		}
+	}
+
+	public final NumberContext number() throws RecognitionException {
+		NumberContext _localctx = new NumberContext(_ctx, getState());
+		enterRule(_localctx, 12, RULE_number);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(34);
+			_la = _input.LA(1);
+			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << DecimalConst) | (1L << OctalConst) | (1L << HexadecimalConst))) != 0)) ) {
+			_errHandler.recoverInline(this);
+			}
+			else {
+				if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+				_errHandler.reportMatch(this);
+				consume();
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\23\'\4\2\t\2\4\3"+
+		"\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\3\2\3\2\3\3\3\3\3\3\3\3\3"+
+		"\3\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\6\3\6\3\7\3\7\3\7\3\7\3\b\3\b\3\b\2\2"+
+		"\t\2\4\6\b\n\f\16\2\3\3\2\f\16\2\37\2\20\3\2\2\2\4\22\3\2\2\2\6\30\3\2"+
+		"\2\2\b\32\3\2\2\2\n\34\3\2\2\2\f \3\2\2\2\16$\3\2\2\2\20\21\5\4\3\2\21"+
+		"\3\3\2\2\2\22\23\5\6\4\2\23\24\5\b\5\2\24\25\7\3\2\2\25\26\7\4\2\2\26"+
+		"\27\5\n\6\2\27\5\3\2\2\2\30\31\7\5\2\2\31\7\3\2\2\2\32\33\7\6\2\2\33\t"+
+		"\3\2\2\2\34\35\7\7\2\2\35\36\5\f\7\2\36\37\7\b\2\2\37\13\3\2\2\2 !\7\t"+
+		"\2\2!\"\5\16\b\2\"#\7\n\2\2#\r\3\2\2\2$%\t\2\2\2%\17\3\2\2\2\2";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
