5296574942a3d51c16053a0c345123dcb7f8fbdd
zzy0202 <57013433+zzy0202@users.noreply.github.com>
2021-10-20 07:19:53 -0700

    Add files via upload

---
+++ ThrowingErrorListener.java
@@ -0,0 +1,16 @@
+package lab1;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+
+public class ThrowingErrorListener extends BaseErrorListener {
+    public static final ThrowingErrorListener Instance = new ThrowingErrorListener();
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+      throws ParseCancellationException{
+        System.exit(114514);
+    }
+}
