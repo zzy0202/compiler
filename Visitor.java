18f4b9ba3f142b49a1d231ced84d21c12ece148c
zzy0202 <57013433+zzy0202@users.noreply.github.com>
2021-11-01 02:25:15 +0800

    Update Visitor.java

--- Visitor.java
+++ Visitor.java
@@ -241,7 +241,7 @@ public class Visitor extends lab3BaseVisitor<Void> {
                         System.exit(22);
                     }
                     Calculator.getAns(exp,true);
-                    System.out.println("\tcall void @putint(i32 %7)");
+                    System.out.println("\tcall void @putint(i32 %8)");
                     exp="";
                 }
 //                else if(ctx.ident1().getText().equals("getch")){
