3e2e6934c71ead20f81ef801fd9acd821bcc0bfe
zzy0202 <57013433+zzy0202@users.noreply.github.com>
2021-11-09 01:30:00 -0800

    Add files via upload

--- CalculatorIfExp.java
+++ CalculatorIfExp.java
@@ -186,10 +186,12 @@ public class CalculatorIfExp {
                 String get="";
                 if(a.charAt(0)=='%'){
                     get=a;
+                    get=get.substring(1);
                     a="var"+get;
                 }
                 if(b.charAt(0)=='%'){
                     get=b;
+                    get=get.substring(1);
                     b="var"+get;
                 }
                 switch (list.get(i)) {
